6c762ce083d27753ea4650545b36a143e27fb11a
raj9039852537 <raj9039852537@gmail.com>
2024-11-21 12:31:36 +0530

    Add 70 styles to shift key rotation

--- app/src/main/java/helium314/keyboard/latin/utils/RecapitalizeStatus.java
+++ app/src/main/java/helium314/keyboard/latin/utils/RecapitalizeStatus.java
@@ -6,6 +6,8 @@
 
 package helium314.keyboard.latin.utils;
 
+import helium314.keyboard.latin.TextTransformer;
+import helium314.keyboard.keyboard.internal.keyboard_parser.KeyboardParser;
 import helium314.keyboard.latin.common.StringUtils;
 
 import java.util.Locale;
@@ -21,13 +23,96 @@ public class RecapitalizeStatus {
     public static final int CAPS_MODE_ALL_UPPER = 3;
     // When adding a new mode, don't forget to update the CAPS_MODE_LAST constant.
     public static final int CAPS_MODE_LAST = CAPS_MODE_ALL_UPPER;
+    private String Transform = KeyboardParser.LAYOUT_TRANSFORMATION;
 
-    private static final int[] ROTATION_STYLE = {
-        CAPS_MODE_ORIGINAL_MIXED_CASE,
-        CAPS_MODE_ALL_LOWER,
-        CAPS_MODE_FIRST_WORD_UPPER,
-        CAPS_MODE_ALL_UPPER
-    };
+    private static final String[] styke= {
+            "ori",
+            "Low",
+            "upp",
+            "1st",
+            "𝐚𝐛𝐜",
+            "𝑎𝑏𝑐",
+            "a̲b̲c̲",
+            "ɔqɒ",
+            "𝒶𝒷𝒸",
+            "𝔞𝔟𝔠",
+            "🅐🅑🅒",//10
+            "a̶b̶c̶",
+            "sub",
+            "sup",
+            "doublevision",
+            "big",
+            "a͛b͛c",
+            "a≋b≋c",
+            "a|b|c",
+            "a.b.c",
+            "a~b~c",
+            "a_b_c",//20
+            "a⊶b⊶c",
+            "a̷b̷c",
+            "a҉b҉c",
+            "a⨳b⨳c",
+            "a⦚b⦚c",
+            "αвς∂εƒgнïʝкℓ๓ท๑ρqяรтบνพχγz",
+            "Aβ⊂DξFGHไĴK£MИΘ℘QЯSTЦปฟжУζ",
+            "ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩⒶⒷⒸⒹⒺⒻⒼⒽⒾⒿⓀⓁⓂⓃⓄⓅⓆⓇⓈⓉⓊⓋⓌⓍⓎⓏ",
+            "ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢ",
+            "𝓪𝓫𝓬𝓭𝓮𝓯𝓰𝓱𝓲𝓳𝓴𝓵𝓶𝓷𝓸𝓹𝓺𝓻𝓼𝓽𝓾𝓿𝔀𝔁𝔂𝔃",
+            "𝕒𝕓𝕔𝕕𝕖𝕗𝕘𝕙𝕚𝕛𝕜𝕝𝕞𝕟𝕠𝕡𝕢𝕣𝕤𝕥𝕦𝕧𝕨𝕩𝕪𝕫",//30
+            "𝔞𝔟𝔠𝔡𝔢𝔣𝔤𝔥𝔦𝔧𝔨𝔩𝔪𝔫𝔬𝔭𝔮𝔯𝔰𝔱𝔲𝔳𝔴𝔵𝔶𝔷",
+            "𝖆𝖇𝖈𝖉𝖊𝖋𝖌𝖍𝖎𝖏𝖐𝖑𝖒𝖓𝖔𝖕𝖖𝖗𝖘𝖙𝖚𝖛𝖜𝖝𝖞𝖟",
+            "🅰🅱🅲🅳🅴🅵🅶🅷🅸🅹🅺🅻🅼🅽🅾🅿🆀🆁🆂🆃🆄🆅🆆🆇🆈🆉",
+            "ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ",
+            //"‌🇦‌🇧‌🇨‌🇩‌🇪‌🇫‌🇬‌🇭‌🇮‌🇯‌🇰‌🇱‌🇲‌🇳‌🇴‌🇵‌🇶‌🇷‌🇸‌🇹‌🇺‌🇻‌🇼‌🇽‌🇾‌🇿",//
+            "🄰🄱🄲🄳🄴🄵🄶🄷🄸🄹🄺🄻🄼🄽🄾🄿🅀🅁🅂🅃🅄🅅🅆🅇🅈🅉",
+            "丹乃匚刀モ下ム卄工ＪＫㄥ爪れ口ㄗＱ尺ち匕∪∨山メㄚ乙",
+            "ᗩᗷᑕᗪᗴᖴǤᕼᎥᒎᛕᒪᗰᑎᗝᑭɊᖇᔕ丅ᑌᐯᗯ᙭Ƴ乙",
+            "ค๒ς๔єŦﻮђเןкl๓ภ๏קợгรtยשฬץאz",
+            "ᏗᏰፈᎴᏋᎦᎶᏂᎥᏠᏦᏝᎷᏁᎧᎮᎤᏒᏕᏖᏬᏉᏇጀᎩፚ",//40
+            "αႦƈԃҽϝɠԋιʝƙʅɱɳσρϙɾʂƚυʋɯxყȥ",
+            "αв¢∂єƒgнιנкℓмησρqяѕтυνωχуz",
+            "ᵃᵇᶜᵈᵉᶠᵍʰⁱʲᵏˡᵐⁿᵒᵖqʳˢᵗᵘᵛʷˣʸᶻ",
+            "ᴬᴮᶜᴰᴱᶠᴳᴴᴵᴶᴷᴸᴹᴺᴼᴾQᴿˢᵀᵁⱽᵂˣʸᶻ",
+            "ǟɮƈɖɛʄɢɦɨʝӄʟʍռօքզʀֆȶʊʋաӼʏʐ",
+            "ɐqɔpǝɟƃɥᴉɾʞlɯuodbɹsʇnʌʍxʎz",
+            "ƸYXWVUTꙄЯỌꟼOͶM⅃⋊ႱIHᎮꟻƎᗡƆᙠA",
+            "ƹʏxwvuƚꙅɿpqoᴎm|ʞꞁiʜǫᎸɘbɔdɒ",
+            "ꍏꌃꉓꀸꍟꎇꁅꃅꀤꀭꀘ꒒ꂵꈤꂦꉣꆰꋪꌗ꓄ꀎꃴꅏꊼꌩꁴ",
+            "ąცƈɖɛʄɠɧıʝƙƖɱŋơ℘զཞʂɬų۷ῳҳყʑ",//50
+            "άвςȡέғģħίјķĻмήόρqŕşţùνώxчž",
+            "ค๒ς๔єŦﻮђเןкɭ๓ภ๏קợгรՇยשฬאץչ",
+            "αႦƈԃҽϝɠԋιʝƙʅɱɳσρϙɾʂƚυʋɯxყȥ",
+            "ǟɮƈɖɛʄɢɦɨʝӄʟʍռօքզʀֆȶʊʋաӼʏʐ",
+            "ᏗᏰፈᎴᏋᎦᎶᏂᎥᏠᏦᏝᎷᏁᎧᎮᎤᏒᏕᏖᏬᏉᏇጀᎩፚ",
+            "ąცƈɖɛʄɠɧıʝƙƖɱŋơ℘զཞʂɬų۷ῳҳყʑ",
+            "ค๖¢໓ēfງhiวkl๓ຖ໐p๑rŞtนงຟxฯຊ",
+            "αზƈԃҽϝɠԋιʝƙʅɱɳσρϙɾʂƚυʋɯxყȥ",
+            "ĂβČĎĔŦĞĤĨĴĶĹМŃŐРQŔŚŤÚVŴЖŶŹ",
+            "ΛϦㄈÐƐFƓнɪﾌҚŁ௱ЛØþҨ尺らŤЦƔƜχϤẔ",//60
+            "ƛƁƇƊЄƑƓӇƖʆƘԼMƝƠƤƢƦƧƬƲƔƜҲƳȤ",
+            "卂乃匚ᗪ乇千Ꮆ卄丨ﾌҜㄥ爪几ㄖ卩Ɋ尺丂ㄒㄩᐯ山乂ㄚ乙",
+            "ﾑ乃ᄃり乇ｷムんﾉﾌズﾚﾶ刀のｱゐ尺丂ｲひ√Wﾒﾘ乙",
+            "คც८ძ૯Բ૭ҺɿʆқՆɱՈ૦ƿҩՐς੮υ౮ω૪עઽ",
+            "ԹՅՇԺȝԲԳɧɿʝƙʅʍՌԾρφՐՏԵՄעաՃՎՀ",
+            "αɓ૮∂εƒɠɦเʝҡℓɱɳσρφ૨รƭµѵωאყƶ",
+            "მჩეძპfცhἶქκlოῆõρგΓჰནυὗwჯყɀ",
+            "ꋬꃳꉔ꒯ꏂꊰꍌꁝ꒐꒻ꀘ꒒ꂵꋊꄲꉣꆰꋪꇙ꓄꒤꒦ꅐꉧꌦꁴ",
+            "ᵃᵇᶜᵈᵉᶠᵍʰⁱʲᵏˡᵐⁿᵒᵖqʳˢᵗᵘᵛʷˣʸᶻ",
+            "абcдёfgнїjкгѫпѳpфя$тцѵщжчз",//70
+            "ДБCDΞFGHIJҜLMИФPǪЯSΓЦVЩЖУZ"
+
+};
+
+    private static final int[] ROTATION_STYLE = new int[styke.length];
+        static {
+            ROTATION_STYLE[0] = CAPS_MODE_ORIGINAL_MIXED_CASE;
+            ROTATION_STYLE[1] = CAPS_MODE_ALL_LOWER;
+            ROTATION_STYLE[2] = CAPS_MODE_FIRST_WORD_UPPER;
+            ROTATION_STYLE[3] = CAPS_MODE_ALL_UPPER;
+            for (int i = 4; i < styke.length; i++) {
+                ROTATION_STYLE[i] = i ;  // This will assign values starting from 3 to 70
+            }
+        }
 
     private static int getStringMode(final String string, final int[] sortedSeparators) {
         if (StringUtils.isIdenticalAfterUpcase(string)) {
@@ -135,6 +220,7 @@ public class RecapitalizeStatus {
      */
     public void rotate() {
         final String oldResult = mStringAfter;
+        TextTransformer textTransformer = new TextTransformer();
         int count = 0; // Protection against infinite loop.
         do {
             mRotationStyleCurrentIndex = (mRotationStyleCurrentIndex + 1) % ROTATION_STYLE.length;
@@ -144,13 +230,14 @@ public class RecapitalizeStatus {
                         (mRotationStyleCurrentIndex + 1) % ROTATION_STYLE.length;
             }
             ++count;
-            switch (ROTATION_STYLE[mRotationStyleCurrentIndex]) {
-                case CAPS_MODE_ALL_LOWER -> mStringAfter = mStringBefore.toLowerCase(mLocale);
-                case CAPS_MODE_FIRST_WORD_UPPER -> mStringAfter = StringUtils.capitalizeEachWord(mStringBefore, mSortedSeparators, mLocale);
-                case CAPS_MODE_ALL_UPPER -> mStringAfter = mStringBefore.toUpperCase(mLocale);
-                default -> mStringAfter = mStringBefore;
-            }
-        } while (mStringAfter.equals(oldResult) && count < ROTATION_STYLE.length + 1);
+                switch(ROTATION_STYLE[mRotationStyleCurrentIndex]){
+                    case 0 -> mStringAfter = mStringBefore;
+                    case 1 -> mStringAfter = mStringBefore.toLowerCase(mLocale);
+                    case 2 -> mStringAfter = StringUtils.capitalizeEachWord(mStringBefore, mSortedSeparators, mLocale);
+                    case 3 -> mStringAfter = mStringBefore.toUpperCase(mLocale);
+                    default -> mStringAfter = textTransformer.transformText(mStringBefore,styke[mRotationStyleCurrentIndex]);
+                }
+       } while (mStringAfter.equals(oldResult) && count < ROTATION_STYLE.length + 1);
         mCursorEndAfter = mCursorStartAfter + mStringAfter.length();
     }
 
